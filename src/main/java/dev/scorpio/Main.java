5166509ee6ce63d9394e80e06bdc741a547c395f
Vaibhav Srivastava1 <vaibhav_srivastava1@epam.com>
2024-08-05 20:54:14 +0530

    write a program to find the second highest occurences of a character in a string using java8

--- src/main/java/dev/scorpio/Main.java
+++ src/main/java/dev/scorpio/Main.java
@@ -1,11 +1,27 @@
 package dev.scorpio;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("just testing java 11 feature..");
+        /*System.out.println("just testing java 11 feature..");
         int[] ints = {21,5,1,7,9,3,12};
         int len = 3;
-        largestSumSubarray(ints, len);
+        largestSumSubarray(ints, len);*/
+        String input = "aabbcccddddeeefffff"; // Example input
+        findSecondHighestOccurrence(input);
+    }
+
+    private static void findSecondHighestOccurrence(String input) {
+        Map<Integer, Long> map = input.chars().boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+//        collect.entrySet().stream().sorted((Comparator<? super Map.Entry<Integer, Long>>) Map.Entry.comparingByValue().reversed()).collect(Collectors.toMap())
+        List<Long> list = map.values().stream().sorted(Comparator.reverseOrder()).distinct().collect(Collectors.toList());
+        Long l = list.get(1);
+        System.out.println(l);
     }
 
     private static void largestSumSubarray(int[] ints, int len) {
